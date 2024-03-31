f40cab16fd87724d2c4227f30e26d64de57a1024
faazlasiddiqui <160077722+faazlasiddiqui@users.noreply.github.com>
2024-03-31 05:59:40 +0500

    simple calculator cli

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter firstn number", type: "number", name: "firstNumber" },
+    { message: "Enter first number", type: "number", name: "firstNumber" },
     { message: "Enter second number:", type: "number", name: "secondnumber" },
     {
         message: "Select one of the operators to perform operation",
--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter firstn number", type: "number", name: "firstNumber" },
+  { message: "Enter first number", type: "number", name: "firstNumber" },
   { message: "Enter second number:", type: "number", name: "secondnumber" },
   {
     message: "Select one of the operators to perform operation",
